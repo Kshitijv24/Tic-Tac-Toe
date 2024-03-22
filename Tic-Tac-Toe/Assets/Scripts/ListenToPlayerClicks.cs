8ec9f8ed99f81264f4249dce308f1a7f717375cb
Kshitijv24 <kshitijv24@gmail.com>
2024-03-22 14:09:10 +0530

    Stopped the player from placing more than one icons on the same block
    and created a separate empty game object to keep all the player placed icons.

--- Tic-Tac-Toe/Assets/Scripts/ListenToPlayerClicks.cs
+++ Tic-Tac-Toe/Assets/Scripts/ListenToPlayerClicks.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class ListenToPlayerClicks : MonoBehaviour
 {
     [SerializeField] GameObject playerIcon;
+    [SerializeField] GameObject playerMoveIcons;
+
+    bool isEmpty = true;
 
     private void OnMouseDown()
     {
-        Instantiate(playerIcon, transform.position, transform.rotation);
+        if (isEmpty)
+        {
+            Instantiate(playerIcon, transform.position, transform.rotation, playerMoveIcons.transform);
+            isEmpty = false;
+        }
     }
 }
